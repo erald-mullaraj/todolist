7e098c271766ff670aa4b656a2c3e145c6bec225
erald-mullaraj <eraldmullaraj@gmail.com>
2022-12-24 19:51:05 +0100

    bug fixed

--- script.js
+++ script.js
@@ -8,35 +8,18 @@ const randomColor = () =>
         `rgb(${randomInteger(0,255)}, ${randomInteger(0,255)},
         ${randomInteger(0,255)})`;
 
- 
 document.addEventListener('DOMContentLoaded', function() {
     //by defaultm submit button is disabled
-
     document.querySelector('#submit').disabled = true;
     document.querySelector('#task').onkeyup = () => {
         if (document.querySelector('#task').value.length > 0) {
             document.querySelector('#submit').disabled = false;
         } else {
             document.querySelector('#submit').disabled = true;
-        }
-    }
+        };
+    };
     postTask ();         
-    });   
-            
-function editTask (){   
-    const element = event.target;
-    toEdit = element.parentElement.innerHTML;
-    var newTask = toEdit.replace(b, '');
-    document.querySelector('form').onsubmit , () => {
-        element.parentElement.style = ' ';
-        const taskValue = document.querySelector('#task').value;
-        element.parentElement.innerHTML = taskValue + a; 
-        document.querySelector('#task').value = '';
-        document.querySelector('#submit').disabled = true;                       
-        // stop form from submitting
-        return false;                   
-        }                       
-    }      
+    });        
 
 const formatedDate = function () {
     const now = new Date();
@@ -61,7 +44,7 @@ const startTimer = function (nr, time) {
         labTimer.textContent = `${hrs}:${min}:${sec}`;
       }  else if (timeSec < 3600) {
         labTimer.textContent = `${min}:${sec}`;
-      }
+      };
       if (timeSec === 0) {
         clearInterval(timer);
         labelPost.style.backgroundColor = "red";
@@ -72,47 +55,22 @@ const startTimer = function (nr, time) {
     labTimer.addEventListener('click', function (e) {
         this.style.backgroundColor = randomColor();
         console.log(randomColor());
-    })
+        console.log(this.sytle.backgroundColor);
+    });
     const timer = setInterval(timeSnap, 1000);
-    //labTimer.addEventListener('click', clearInterval(timer))
     return timer;
   };
 
-const editPost = function (nr, elm) {
-    const tskInp = document.querySelector(`.taskInput-${nr}`); 
-    const editLabel = document.querySelector(`#editId-${nr}`); 
-    console.log(editLabel) 
-    if (editLabel.textContent === 'Edit'){
-        console.log(tskInp.contentEditable + '1 here')
-        tskInp.contentEditable = true;
-        //element.parentElement.contentEditable = true;
-        elm.parentElement.style = 'background-color: #68a1b0;'; 
-        editLabel.textContent = 'Confirm';
-        console.log(tskInp.contentEditable + '2 here')  
-        } else {
-            console.log(tskInp.contentEditable + '3 here')
-            editLabel.textContent = 'Edit';
-            tskInp.contentEditable = false;
-            elm.parentElement.style = ' ';
-            console.log(tskInp.contentEditable + '4 here')
-        }   
-}
-  
 function postTask (){
     document.querySelector('form').onsubmit = () => {
         taskNr++;
-        // if (time = document.querySelector('#taskTime').value) {
-        //     time;
-        // } else {
-        //     var time = 300;
-        // }
         time =(time = document.querySelector('#taskTime').value) ? time : 30;
         const taskValue = document.querySelector('#task').value;
         const post = document.createElement('div');
         post.className = `post-${taskNr} post `;
         post.innerHTML = 
         `<div class="date">${formatedDate()}</div>
-        <div class="taskInput-${taskNr}">${taskNr}. ${taskValue}</div>
+        <div class="taskTxt">${taskNr}. ${taskValue}</div>
         <button class="edit" id="editId-${taskNr}">Edit</button>
         <button class="delete">Delete</button>
         <button class="timer timer-${taskNr}">1:00</button>`;     
@@ -122,9 +80,9 @@ function postTask (){
         document.querySelector('#submit').disabled = true;                      
         // stop form from submitting
         startTimer(taskNr, time);
-        return false
-    }   
-}
+        return false;
+    }; 
+};
            
  // If hide button is clicked, delete the post
 document.addEventListener('click', event => {
@@ -135,12 +93,16 @@ document.addEventListener('click', event => {
             element.parentElement.remove();
             });
         } else  if (element.className === 'edit'  ){ 
-            editPost(taskNr, element);
-        // } else if (element.className === 'confirm') {
-        //     console.log(tskInp.contentEditable)
-        //     tskInp.contentEditable = false;
-        //     element.parentElement.style = ' ';
+            if (element.textContent === 'Edit'){
+                element.previousElementSibling.contentEditable = true;
+                element.parentElement.style = 'background-color: #68a1b0;'; 
+                element.textContent = 'Confirm'; 
+            } else {
+                element.textContent = 'Edit';
+                element.previousElementSibling.contentEditable = false;
+                element.parentElement.style = ' ';
+            };
         } else if (element.className === 'timer') {
             startTimer(taskNr, time); 
-        }   
+        };   
     });
